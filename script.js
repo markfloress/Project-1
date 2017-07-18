642ca5805d2f0f858f8f3a8f16a5fa6372801a43
Mark <mark@Marks-MacBook-Pro.local>
2017-07-18 03:45:34 -0700

    Added the rest of the project

---
+++ script.js
@@ -0,0 +1,12 @@
+$('.main-carousel').flickity({
+  // options
+  cellAlign: 'left',
+  contain: true,
+  autoPlay: 3000
+});
+
+window.scroll({
+  top: 2500, 
+  left: 0, 
+  behavior: 'smooth' 
+});
