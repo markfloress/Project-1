ad326a24a71624790a50ab7f74e76e3fe52298ed
Mark <mark@Marks-MacBook-Pro.local>
2017-07-26 16:26:20 -0700

    added Skip Link

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-$('.main-carousel').flickity({
-  // options
-  cellAlign: 'left',
-  contain: true,
-  autoPlay: 3000
-});
+$( document ).ready(function() {
+  $('.main-carousel').flickity({
+    cellAlign: 'left',
+    contain: true,
+    autoPlay: 3000
+  });
 
-window.scroll({
-  top: 2500, 
-  left: 0, 
-  behavior: 'smooth' 
+  window.scroll({
+    top: 2500, 
+    left: 0, 
+    behavior: 'smooth' 
+  });
+
+  $(function() {
+    $("a[href^='#']").not("a[href='#']").click(function() {
+        $("#"+$(this).attr("href").slice(1)+"").focus();
+    });
+  });
 });
