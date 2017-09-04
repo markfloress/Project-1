43c0040a5c858bf74bd90c1e586d6cdb73e1c2d6
Mark <mark@Marks-MacBook-Pro.local>
2017-09-04 15:31:41 -0700

    cleaned up some bugs from HTML and changes scroll offset

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $( document ).ready(function() {
           event.preventDefault();
           var hash = this.hash;
           $('html, body').animate({
-            scrollTop: $(hash).offset().top - 80
+            scrollTop: $(hash).offset().top - 115
           }, 700, function(){
             window.location.hash = hash;
           });
