58d35cf7c88fe1d26a8b93d649a6e7c5c5701a92
Mark <mark@Marks-MacBook-Pro.local>
2017-09-08 10:03:06 -0700

    added else alert if invalid email

--- script.js
+++ script.js
@@ -22,6 +22,8 @@ $( document ).ready(function() {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if(re.test($('#email').val())){
       alert('Thank you for subscribing!');
+    } else {
+      alert('Please enter a valid email address');
     }
    })
 });
